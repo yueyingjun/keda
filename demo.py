3dfa96cde31b365bd47e00190022c7a95884156c
yueyingjun <111111@qq.com>
2019-01-02 15:23:00 +0800

    aa

---
+++ demo.py
@@ -0,0 +1,31 @@
+name="岳英俊"
+age=18
+like={"one":"学习","two":"游泳","three":"打篮球"}
+print(type(like))
+# 字符串  整形  浮点型   列表   元组   字典
+
+# 分支   循环
+num=11
+if num>10:
+    print("大于10")
+elif num<10:
+    print("小于10")
+else:
+    print("等于10")
+
+"操场上一共有100-200人，3一行  1  ，4一行 2,5一行 3  " \
+" 鸡兔    35个头  94只脚  问 鸡 兔各有多少只 "
+
+
+# for item in range(100,200):
+#     if item%3==1 and item%4==2 and item%5==3:
+#         print(item)
+
+# for tu in range(0,36):
+#     ji=35-tu
+#     if tu*4+ji*2==94:
+#         print(tu,ji)
+
+
+
+
